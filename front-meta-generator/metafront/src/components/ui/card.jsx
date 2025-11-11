9c543e608f81f52716a4b03bb7d1a9147749646d
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2025-11-11 16:56:29 -0300

    front incial do gerador de entidades

---
+++ front-meta-generator/metafront/src/components/ui/card.jsx
@@ -0,0 +1,10 @@
+// src/components/ui/card.jsx
+import React from "react";
+
+export function Card({ children, className }) {
+  return <div className={`p-4 bg-white rounded shadow ${className || ""}`}>{children}</div>;
+}
+
+export function CardContent({ children, className }) {
+  return <div className={`p-2 ${className || ""}`}>{children}</div>;
+}
